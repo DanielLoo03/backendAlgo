a1280de31ca9cb3f09c7e610c7d160ec8008c044
DanielLoo03 <dloog23@gmail.com>
2025-07-20 21:58:36 -0700

    Se agrega linea en index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: function(origin, callback) {
     const allowedOrigins = [
+      'https://danielloo03.github.io',
       'https://danielloo03.github.io/algo',
       'https://danielloo03.github.io/algo/',
     ];
